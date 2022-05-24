967ed5816530628b4b0ddb52238731478048a21b
V15hnu24 <799vishnu530@gmail.com>
2022-05-24 17:14:20 +0530

    Commit

--- Selenium/src/LaunchBrowser.java
+++ Selenium/src/LaunchBrowser.java
@@ -11,21 +11,25 @@ public class LaunchBrowser {
     public static WebDriver driver = null;
 
     public static void main(String[] args) throws InterruptedException {
+        //Connect to the browser
         System.setProperty("webdriver.chrome.whitelistedIps", "");
         System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver_win32\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
+        //Open website
         driver.navigate().to("https://Amazon.in");
         driver.manage().window().maximize();
         String title = driver.getTitle();
 
+        //Validate the title
         if (title.equalsIgnoreCase("Amazon.in")){
             System.out.println("Title matches");
         }else{
             System.out.println(title);
         }
 
+        //Select the site component using css selector
         String tagname = "";
         tagname = driver.findElement(By.cssSelector("#searchDropdownBox")).getText();
         System.out.println(tagname);
